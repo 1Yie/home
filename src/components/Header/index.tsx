7ddc5d26bb12643da57f20abf67bf7728769200b
ichiyo <ichiyo@ingstar.moe>
2025-02-22 16:34:23 +0800

    Header 增加移动设备优化

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState  } from "react";
 import style from "./header.module.css";
 import { Link, useLocation } from "react-router-dom";
 
@@ -24,35 +24,44 @@ const headerList = [
 
 const Header: React.FC = () => {
   const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <>
-      <header className={style.header}>
-        <section>
-          <div className={style.headerContainer}>
-            <a href={headerName.link} className="HtmlLogo">
-              {headerName.name}
-            </a>
-            <nav>
-              <ul>
-                {headerList.map((item, index) => (
-                  <li key={index}>
-                    <Link
-                      to={item.link}
-                      className={
-                        location.pathname === item.link ? style.active : ""
-                      }
-                    >
-                      {item.name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
-        </section>
-      </header>
-    </>
+    <header className={style.header}>
+      <section>
+        <div className={style.headerContainer}>
+          <a href={headerName.link} className="HtmlLogo">
+            {headerName.name}
+          </a>
+          
+          <button 
+            className={style.menuButton}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            aria-label="Toggle navigation"
+          >
+            ☰
+          </button>
+
+          <nav className={`${style.nav} ${isMenuOpen ? style.navActive : ""}`}>
+            <ul className={style.navList}>
+              {headerList.map((item, index) => (
+                <li key={index}>
+                  <Link
+                    to={item.link}
+                    className={
+                      location.pathname === item.link ? style.active : ""
+                    }
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </section>
+    </header>
   );
 };
 
